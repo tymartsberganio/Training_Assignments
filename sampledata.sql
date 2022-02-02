255a262d03222744e51cf6c6c18ced26a77c60da
Marty Berganio <98569919+tymartsberganio@users.noreply.github.com>
2022-02-02 22:35:55 +0800

    Add files via upload

---
+++ sampledata.sql
@@ -0,0 +1,13 @@
+--Example Insert Admin--
+insert into ADMIN (admin_id,admin_name,password) 
+values('loborer005','LARRY','1234567');
+--Example Insert Users--
+insert into USERS (user_name, email,password,phone_number,address,date_of_registration) 
+values('Larry_oborer','larry@.com','1234567','09123456789','Manila','02-FEB-2022');
+--Example Insert Categories--
+insert into categories(category_name)
+values('elect_dev');
+--Example Insert Product--
+insert into product(product_name,product_price,product_image,product_available_qty)
+values('Techno_cam7','7000','pic.jpg',3);
+
